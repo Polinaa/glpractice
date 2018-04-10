85faea392a9d78599dfdca442f1fec036140285a
Polina Kvachuk <Polina_Kvachuk@epam.com>
2018-04-10 18:28:48 +0300

    test

---
+++ FlowerGarden/src/test/java/com/flowergarden/dao/GeneraFlowerDaoTest.java
@@ -0,0 +1,26 @@
+package com.flowergarden.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class GeneraFlowerDaoTest {
+
+    @Test
+    public void findFlowerById() {
+
+    }
+
+    public void  findAllFlowers() {
+
+    }
+    public void  findFlowersInBouquet() {
+
+    }
+    public void  saveFlower()  {
+
+    }
+    public void  deleteFlowerById(){
+    }
+}
